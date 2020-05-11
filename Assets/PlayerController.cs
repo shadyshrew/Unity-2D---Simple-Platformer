45e8f3646382e98acb1985df5ad032152c18bbc6
ShadyShrew <shrey.uzumaki1@gmail.com>
2020-05-10 22:49:56 -0400

    Added FSM

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,45 +1,72 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D player;
     private Animator anim;
+    private enum State { idle, running, jumping, falling};
+    private State state = State.idle;
+    private Collider2D coll;
+    [SerializeField] private LayerMask ground;
     void Start()
     {
         player = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        coll = GetComponent<Collider2D>();
     }
     private void Update() {
         float hdirection = Input.GetAxis("Horizontal");
-        float vdirection = Input.GetAxis("Vertical");
         if (hdirection < 0)
         {
             player.velocity = new Vector2(-5, player.velocity.y);
             transform.localScale = new Vector2(-1,1);
-            anim.SetBool("running", true);
         }
         else if (hdirection > 0)
         {
             player.velocity = new Vector2(5, player.velocity.y);
             transform.localScale = new Vector2(1, 1);
-            anim.SetBool("running", true);
         }
         else
         {
-            anim.SetBool("running", false);
+           
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
         {
-            player.velocity = new Vector2(player.velocity.x, 27);
+            player.velocity = new Vector2(player.velocity.x, 10f);
+            state = State.jumping;
         }
-        if (Input.GetKey(KeyCode.S))
+        VelocityState();
+        anim.SetInteger("state",(int)state);
+    }
+
+    private void VelocityState()
+    {
+        if (state == State.jumping)
         {
-            player.velocity = new Vector2(player.velocity.x, -5);
+            if(player.velocity.y < 0.1f)
+            {
+                state = State.falling;
+            }
+        }
+        else if(state == State.falling)
+        {
+            if (coll.IsTouchingLayers(ground))
+            {
+                state = State.idle;
+            }
+        }
+        else if (Mathf.Abs(player.velocity.x) > 2f)
+        {
+            state = State.running;
+
+        }
+        else {
+            state = State.idle;
         }
     }
-    // Start is called before the first frame update
 
 }
